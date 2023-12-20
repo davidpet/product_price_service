673df31957122e0eddb07c2612513ee3040fd4de
David Petrofsky <davidpet@ucla.edu>
2023-12-19 23:08:55 -0800

    Adding initial Flask app for the API, with lots of ToDos.
    In-memory array with slow lookup, intended to be expanded.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Product Price Service
+## Status
+Currently only uses in-memory array of all request objects and does a slow O(n^2) search to find the lowest.
+
+## Running
+1. `cd` into this folder
+1. `python3 app.py`
+1. Ctrl/cmd-click the link from the terminal to open in Chrome (will be "broken" link)
+
+## Testing
+1. Run as above
+1. To test `findPrice()`, append `/find-price/{sku}`, where `{sku}` should be some product sku, to the url
+1. To test `receive()`, run this in Chrome DevTools console, replacing fields of `body` and changing the url if different:
+```JavaScript
+fetch("http://127.0.0.1:5000/receive", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "max-age=0",
+    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "content-type": "application/json",
+  },
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": JSON.stringify({
+      sku: '123',
+      retailer: 'bob\'s furniture',
+      price: 172.9,
+      url: 'whatever.com',
+  }),
+  "method": "PUT",
+  "mode": "cors",
+  "credentials": "include"
+});
+```
+NOTE: this is obtained by doing the GET request and then doing __Copy -> Copy as Fetch__ and modifying the url, body, method, and content-type.
---
+++ app.py
@@ -0,0 +1,73 @@
+# TODO: docstrings
+# TODO: type annotations
+# TODO: format
+# TODO: unit tests
+# TODO: make sure README.md is all up to date and finalized
+# TODO: document dependencies (anything that needs to be pip installed or configured on system)
+#       Flask
+# TODO: consider making a manual testing app that uses 'requests' instead of devtools hacking with fetch
+
+# TODO: consider the case of being able to do price history analysis
+#       probably requires an ID field and timestamp added to the DB schema
+#       then tweak the primary key and index structure
+#       query would be for either sku or sku+retailer and get back sorted history list (maybe another index)
+# TODO: schema, index, and rationale strategy (separet doc)
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# PriceUpdate (comes into receive())
+#   retailer: str
+#   sku: str
+#   price: float (should be nonnegative and possibly cut to 2 decimal places)
+#   url: str|None
+# ProductPrice (comes out of findPrice())
+#   exactly same fields as PriceUpdate
+# Schema
+#   PriceUpdate structure above
+#   primary key = sku
+#   index = sku+price
+
+# TODO: fill this out more as go along and at the end
+# Assumptions
+#   1. sku is unique
+#   2. assumed float for prices because more realistic (though examples are in dollars only)
+#   3. none of the examples show spaces in sku or retailer, but assuming they are OK
+#   4. retailer and sku strings are case sensitive and won't be duplicated in a mismatched way
+
+# TODO: make this real and configurable (inc. mirroring, in-memory, and unit testing)
+#   not required to be fully implemented - at least comment what it should do
+# TODO; separate the DB logic from the HTTP logic
+# TODO: consider if this can be made safer for the gunicorn (multiprocess) case
+db = []
+
+@app.route('/receive', methods=['PUT'])
+def receive():
+    data = request.json
+    # TODO: error handling for missing/invalid fields
+    # TODO: possibly convert skus and/or retailers to lowercase for consistent comparisons
+
+    # TODO: handle duplicate prices for same sku+retailer properly
+    db.append(data)
+
+    return '', 204 # nothing to return (void)
+
+# TODO: separate the business logic from the HTTP logic
+#       findPrice() in original spec, find_price in Python style
+# TODO: respond to web-scale traffic (under 20 ms)
+@app.route('/find-price/<string:sku>', methods=['GET'])
+def find_price(sku):
+    # TODO: verify the sku format
+    # TODO: possibly convert sku to lowercase for consistent comparison
+    # TODO: make this more efficient (even for the simulated version)
+    prices = [product_price for product_price in db if product_price['sku'] == sku]
+    if not prices:
+        return jsonify({"message": "Product not found"}), 404
+    lowest_price = min(prices, key = lambda price: price['price'])
+
+    return jsonify(lowest_price) # return ProductPrice object
+
+# TODO: consider port configuration, etc.
+if __name__ == '__main__':
+    app.run(debug=True)
