f203856d06f16ab67323ffff69ea3ca80559113e
David Petrofsky <davidpet@ucla.edu>
2023-12-20 16:45:13 -0800

    Refactoring so that the business logic is in the base.

--- storage_strategy.py
+++ storage_strategy.py
@@ -16,13 +16,38 @@ class StorageStrategy:
         raise NotImplementedError
     
     def update_price(self, api_record):
-        raise NotImplementedError
+        self.__update_history_table(api_record)
+        self.__update_latest_table(api_record)
+        self.__update_lowest_table(api_record)
     
     def lowest_price(self, sku):
         raise NotImplementedError
     
     def debug_info(self):
         raise NotImplementedError
+    
+    def __update_history_table(self, api_record):
+        history_record = HistoryRecord(id = self.next_history_table_id,
+                                       timestamp = datetime.utcnow(),
+                                       **asdict(api_record))
+        self._update_history_table(history_record)
+
+    def __update_latest_table(self, api_record):
+        latest_price_record = LatestPriceRecord(**asdict(api_record))
+        self._update_latest_table(latest_price_record)
+
+    def __update_lowest_table(self, api_record):
+        sku, retailer, price = api_record.sku, api_record.retailer, api_record.price
+
+        current_entry = self._lowest_price_table_entry(sku)
+        if not current_entry:
+            self._create_lowest_price_entry(LowestPriceRecord(**asdict(api_record)))
+        else:
+            if price < current_entry.price:
+                self._update_lowest_price_entry(LowestPriceRecord(**asdict(api_record)))
+            elif price > current_entry.price and current_entry.retailer == retailer:
+                lowest_price_point = self._query_lowest_price_point(sku)
+                self._update_lowest_price_entry(LowestPriceRecord(**asdict(lowest_price_point)))
 
 class ManualTestingStorageStrategy(StorageStrategy):
     def __init__(self):
@@ -38,11 +63,6 @@ class ManualTestingStorageStrategy(StorageStrategy):
     def end_transaction(self):
         pass
 
-    def update_price(self, api_record):
-        self.__update_history_table(api_record)
-        self.__update_latest_table(api_record)
-        self.__update_lowest_table(api_record)
-
     def lowest_price(self, sku):
         if sku in self.lowest_price_table:
             return APIRecord(**asdict(self.lowest_price_table[sku]))
@@ -54,27 +74,23 @@ class ManualTestingStorageStrategy(StorageStrategy):
                 'latest': list(self.latest_price_table.values()),
                 'lowest': list(self.lowest_price_table.values())}
 
-    def __update_history_table(self, api_record):
-        history_record = HistoryRecord(id = self.next_history_table_id,
-                                       timestamp = datetime.utcnow(),
-                                       **asdict(api_record))
+    def _update_history_table(self, history_record):
         self.next_history_table_id += 1
         self.history_table[history_record.id] = history_record
 
-    def __update_latest_table(self, api_record):
-        latest_price_record = LatestPriceRecord(**asdict(api_record))
+    def _update_latest_table(self, latest_price_record):
         self.latest_price_table[latest_price_record.sku,latest_price_record.retailer] = latest_price_record
-
-    def __update_lowest_table(self, api_record):
-        sku, retailer, price = api_record.sku, api_record.retailer, api_record.price
-
-        if sku not in self.lowest_price_table:
-            self.lowest_price_table[sku] = LowestPriceRecord(**asdict(api_record))
-        else:
-            current_entry = self.lowest_price_table[sku]
-            if price < current_entry.price:
-                self.lowest_price_table[sku] = LowestPriceRecord(**asdict(api_record))
-            elif price > current_entry.price and current_entry.retailer == retailer:
-                price_points = [self.latest_price_table[key] for key in self.latest_price_table if key[0] == sku]
-                lowest_price_point = min(price_points, key = lambda p: p.price)
-                self.lowest_price_table[sku] = LowestPriceRecord(**asdict(lowest_price_point))
+    
+    def _lowest_price_table_entry(self, sku):
+        return self.lowest_price_table.get(sku, None)
+    
+    def _create_lowest_price_entry(self, lowest_price_record):
+        self.lowest_price_table[lowest_price_record.sku] = lowest_price_record
+    
+    def _update_lowest_price_entry(self, lowest_price_record):
+        self.lowest_price_table[lowest_price_record.sku] = lowest_price_record
+    
+    def _query_lowest_price_point(self, sku):
+        price_points = [self.latest_price_table[key] for key in self.latest_price_table if key[0] == sku]
+        return min(price_points, key = lambda p: p.price)
+    
