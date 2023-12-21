f854c24f06d25ca4800f6fd57b123f8173202869
David Petrofsky <davidpet@ucla.edu>
2023-12-20 19:51:10 -0800

    Fixing lint errors.

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 """Main Flask WSGI application hosting Product Price Service hooks."""
 
-# TODO: format+lint
-# TODO: unit tests
-
 from flask import Flask, jsonify, request
 
 from schema import APIRecord
@@ -32,13 +29,13 @@ def receive():
 
     data = request.json
     if not 'sku' in data or not data['sku'].strip():
-        return jsonify({"message": "Missing sku"}), 400
+        return jsonify({'message': 'Missing sku'}), 400
     if not 'retailer' in data or not data['retailer'].strip():
-        return jsonify({"message": "Missing retailer"}), 400
+        return jsonify({'message': 'Missing retailer'}), 400
     if not 'price' in data:
-        return jsonify({"message": "Missing price"}), 400
+        return jsonify({'message': 'Missing price'}), 400
     if data['price'] < 0:
-        return jsonify({"message": "Price cannot be negative"}), 400
+        return jsonify({'message': 'Price cannot be negative'}), 400
 
     api_record = APIRecord(sku=data['sku'].lower(),
                            retailer=data['retailer'].lower(),
@@ -51,7 +48,9 @@ def receive():
 
     cache_strategy.invalidate(api_record.sku)
 
-    return '', 204  # nothing to return (void)(user doesn't need to know ID of history table ever)
+    # nothing to return (void)
+    # (user doesn't need to know ID of history table ever)
+    return '', 204
 
 
 @app.route('/find-price/<string:sku>', methods=['GET'])
@@ -79,7 +78,7 @@ def find_price(sku):
     if result:
         return jsonify(result)
     else:
-        return jsonify({"message": "Product not found"}), 404
+        return jsonify({'message': 'Product not found'}), 404
 
 
 # TODO: block this behind an environment flag
